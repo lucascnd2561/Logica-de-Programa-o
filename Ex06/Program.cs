4e1db6d2683a5a4d443ce9b683d70e56d40aae26
LUCAS DA SILVA CAMILO <lucas.scamilo2@senacsp.edu.br>
2025-01-15 20:24:57 -0300

    primeiro commit

---
+++ Ex06/Program.cs
@@ -0,0 +1,18 @@
+﻿// Solicitar dois numeros ao usuario e realizr texto logico
+
+Console.Write("N1");
+double n1 = double.Parse
+    (Console.ReadLine());
+
+
+Console.Write("n2");
+double n2 = double.Parse
+    (Console.ReadLine());
+
+Console.WriteLine($"N1 é maior que n2? RWaposta {n1 > n2}");
+Console.WriteLine($"n1 é maior ou igual que n2? Resposta {n1 <= n2}");
+Console.WriteLine($"n1 é menor que n2? Resposta {n1 <= n2}");
+Console.WriteLine($"n1 é igual que n2? : Resposta {n1 == n2}");
+Console.WriteLine($"n1 é diferente que n2? Resposta: {n1 != n2}");
+Console.WriteLine($"n1 ou n2 são mairo que zero? Resposta: {(n1 > 0 || n2 > 0)}");
+Console.WriteLine($"n1 e n2 são maior que zero? Resposta: {(n1 > 0 && n2 > 0)}");
---
+++ ex05/Program.cs
@@ -0,0 +1,20 @@
+﻿// calcular que receba 2 numeros e apresente + - * /
+Console.WriteLine("informe o 1° número:");
+double num1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("informe o 2° número:");
+double num2 = double.Parse(Console.ReadLine());
+
+Console.Clear();
+Console.WriteLine("soma : " + (num1 + num2));
+Console.WriteLine("subtração : " + (num1 - num2));
+Console.WriteLine("multiplicação : " + (num1 * num2));
+
+if( num2 != 0)
+{
+    Console.WriteLine("divisão : " + (num1 / num2));
+}
+else
+{
+    Console.WriteLine("divisão por ZERO não permitida");
+}
